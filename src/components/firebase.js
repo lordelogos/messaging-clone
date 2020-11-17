ded1bf1402bc81069dccbd1cbef26cf4749127bf
Paul Ehikhuemen <paulehiks@gmail.com>
2020-11-17 21:10:10 +0100

    initial commit

---
+++ src/components/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCK11znnAYMg4gpGEdrOkQHjkXNhF7x-Ss",
+	authDomain: "whatsapp-clone-beb5d.firebaseapp.com",
+	databaseURL: "https://whatsapp-clone-beb5d.firebaseio.com",
+	projectId: "whatsapp-clone-beb5d",
+	storageBucket: "whatsapp-clone-beb5d.appspot.com",
+	messagingSenderId: "192437944795",
+	appId: "1:192437944795:web:034e2d407628a393d367fe",
+	measurementId: "G-QZWDBPRMVY",
+};
